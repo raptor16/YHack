ee0522f031834cff9d9e491ca05e6839cb1a196e
Ubuntu <azureuser@flywithdrake.flywithdrake.j3.internal.cloudapp.net>
2015-11-07 17:02:30 +0000

    updated everything

--- twittersearchjson.py
+++ twittersearchjson.py
@@ -1,18 +1,11 @@
-import json
 import requests
 url = "http://api.twitter.com/1.1/search/tweets.json?q=from%3AXXX%20%23vacation&result_type=popular"
 # searches for hastag vacation from user XXX
 results = requests.get(url)
-<<<<<<< HEAD
-#jsonresults = results.json
-=======
 jsonresults = results.json
->>>>>>> master
 
-with open('tweet.json', 'w') as outfile:
-    json.dump(results, outfile)
 #for post in jsonresults['results']:
-#	print post['text'].encode('utf-8')s
+#print post['text'].encode('utf-8')s
 
 
 
