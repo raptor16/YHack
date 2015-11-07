4ed044169c229305ca6c94ca340c27f18d283a14
Florence Chan <florence@FlorenceChans-MacBook-Pro.local>
2015-11-06 23:08:30 -0500

    twitterstuff

---
+++ twittersearch.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+import pprint
+
+client = MongoClient ("mongodb://localhost:27017")
+db = client.twitter
+
+def tweetbynameandvacation()
+	result = db.tweets.aggregate([
+				{ "$group" : { "-id" : "$user.screen_name", 
+								"_id" : {"entities.hashtags.vacation"}}}])
+	return result
+
+if __name__ == '__main__':
+	result = tweetbynameandvacation()
+	pprint.pprint(result)
+	
