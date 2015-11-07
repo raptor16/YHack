4cc5e3f5a8ce8c60ec6b8908c08bb78b3c9b38c7
Florence Chan <florence@FlorenceChans-MacBook-Pro.local>
2015-11-07 08:55:03 -0500

    twitter json fix

--- Instagram_self_feed.py
+++ Instagram_self_feed.py
@@ -1,6 +1,6 @@
 import urllib
 
-get_url = "https://api.instagram.com/v1/users/self/feed?access_token=1406078bbd424acc8fc8095f881aa95a"
+get_url = "https://api.instagram.com/v1/users/self/feed?access_token=19425967.1fb234f.6b478a9fab2b49ba9ba479554199a156&max_id=1112658542408961440_38671474"
 resultI = urllib.urlopen(get_url).read()
 
 #print resultI
--- twittersearchjson.py
+++ twittersearchjson.py
@@ -1,10 +1,14 @@
 import requests
-url = 'http://search.twitter.com/search.json?q=%23wholeworldlaughs'
+url = "http://api.twitter.com/1.1/search/tweets.json?q=from%3AXXX%20%23vacation&result_type=popular"
+# searches for hastag vacation from user XXX
 results = requests.get(url)
-jsonresults =- results.json
+jsonresults = results.json
 
 #for post in jsonresults['results']:
-#	print post['text'].encode('utf-8')
+#	print post['text'].encode('utf-8')s
 
 
 
+text_file = open ("twitterSelfFeed.json", "w")
+text_file.write (jsonresults)
+text_file.close()
