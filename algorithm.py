f63b488af7df980ed76efab7e25e39af7c44007f
Florence Chan <florence@FlorenceChans-MacBook-Pro.local>
2015-11-07 17:31:34 -0500

    algorithm first commit

---
+++ algorithm.py
@@ -0,0 +1,71 @@
+places = ["New York", "Grenada", "New Orleans", "Barados", "Bahamas", "Fort Lauderdale", "Liberia", "AZS", "San Diego", "Montego", "Saint Maarten", "Puero Plata", "St. Lucia", "Aruba", "Bermuda", "Curacao", "Tampa", "Grand Cayman", "San Juan", "Charleston", "Cancun", "West Palm Beach", "Orlando", "Turks and Caicos Islands", "Santo Domingo", "Punta Cana", "La Romana", "San Francisco", "Los Angeles", "Fort Myers", "Las Vegas")
+
+
+if age>35: 
+# give smaller distances for points
+#give points to more relazing vacation
+#most points is 15
+	places[1] += 15
+	places[2] += 
+	places[3] += 
+	places[4] += 
+	places[5] += 
+	places[6] += 
+	places[7] += 
+	places[8] += 
+	places[9] += 
+	places[10] += 
+	places[11] += 
+	places[12] += 
+	places[13] += 
+	places[14] += 
+	places[15] += 
+	places[16] += 
+	places[17] += 
+	places[18] += 
+	places[19] += 
+	places[20] +=
+	places[21] += 
+	places[22] += 
+	places[23] += 
+	places[24] += 
+	places[25] += 
+	places[26] += 
+	places[27] += 
+	places[28] += 
+	places[29] += 
+	places[30] += 
+	places[31] += 
+	
+	
+
+else 
+# give greater distances more points 
+# give points to more adventure and small points to relaxing
+
+if gender == "female":
+	#give warm destinations points
+	#most points is 15
+else:
+	#give sportier coler destinations more points
+
+if timezone = "":
+	# identify western then give points to camping and skiing trips
+	# most points is 10
+else if timezone = "":
+	# identify eastern and give points to warm and sunny
+else if timezone = "":
+	# idnentify midwest, give points to warm and sunny
+else:
+	# go to a cruise
+
+if locale != lastnamecountry: # repeat this until you finish all 15 of the destinations
+	#match lastnamecountry with the country list
+	# give points to last name country
+	# most points is 10
+
+if locales in places:
+	places[] = 0
+
+
+#compare countries for which country has the greatest points
